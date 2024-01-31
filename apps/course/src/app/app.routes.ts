e569f05749aa74230abf047ab71b8ffbaa827110
Daniel Jancar <danieljancar@bluewin.ch>
2024-01-31 14:07:32 +0100

    feat(course): create routing for main pages

--- apps/course/src/app/app.routes.ts
+++ apps/course/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CoursesOverviewComponent } from './features/courses-overview/courses-overview.component';
+import { CourseDetailComponent } from './features/course-detail/course-detail.component';
+import { HomepageComponent } from './features/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'c',
+    component: CoursesOverviewComponent,
+  },
+  {
+    path: 'c/:courseId',
+    component: CourseDetailComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- apps/course/src/app/features/courses-overview/courses-overview.component.ts
+++ apps/course/src/app/features/courses-overview/courses-overview.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarCoursesOverviewComponent } from '../../shared/navbar-courses-overview/navbar-courses-overview.component';
 
 @Component({
   selector: 'app-courses-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarCoursesOverviewComponent,
+  ],
   templateUrl: './courses-overview.component.html',
   styleUrl: './courses-overview.component.scss',
 })
