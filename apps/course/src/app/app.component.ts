51c5908ff35981f1c3bbb04ee6f0350b4908be68
Daniel Jancar <danieljancar@bluewin.ch>
2024-01-31 13:39:19 +0100

    feat(course): add tailwind and configs

--- apps/course/src/app/app.component.ts
+++ apps/course/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatChip } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatChip],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
