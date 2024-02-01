c38e612c5a339b8c7c1db396a629fecc4412023a
Daniel Jancar <danieljancar@bluewin.ch>
2024-02-01 09:49:25 +0100

    chore(course): remove empty constructor

--- apps/course/src/app/app.component.ts
+++ apps/course/src/app/app.component.ts
@@ -8,6 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
