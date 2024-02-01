42ea864a357d1273f7eff0b438a7dc7d99e997d6
Kunz Robert <robert_kunz@sluz.ch>
2024-02-01 09:02:09 +0100

    Create a simple Navbar for the courses Tab

--- apps/course/src/app/shared/navbar-courses-overview/navbar-courses-overview.component.ts
+++ apps/course/src/app/shared/navbar-courses-overview/navbar-courses-overview.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar-courses-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './navbar-courses-overview.component.html',
   styleUrl: './navbar-courses-overview.component.scss',
 })
