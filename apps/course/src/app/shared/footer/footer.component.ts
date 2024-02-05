80eec47f71b53edbb29e5f2347109d419e921ed8
Kunz Robert <robert_kunz@sluz.ch>
2024-02-05 15:11:32 +0100

    feat(course): create footer

--- apps/course/src/app/shared/footer/footer.component.ts
+++ apps/course/src/app/shared/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+}
