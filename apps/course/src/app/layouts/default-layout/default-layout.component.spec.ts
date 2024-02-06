66f3efc9824e0b4dd7206f02003009c5cfa1110a
Daniel Jancar <danieljancar@bluewin.ch>
2024-02-06 14:12:19 +0100

    feat(course): restructure navbar definition with layouts

---
+++ apps/course/src/app/layouts/default-layout/default-layout.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DefaultLayoutComponent } from './default-layout.component';
+
+describe('DefaultLayoutComponent', () => {
+  let component: DefaultLayoutComponent;
+  let fixture: ComponentFixture<DefaultLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DefaultLayoutComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DefaultLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
