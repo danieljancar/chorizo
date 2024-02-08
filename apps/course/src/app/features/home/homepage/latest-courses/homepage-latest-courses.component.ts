ce0576e6218c70b23f5891d8cbb18b12f7ef0653
reymacstar <vale.matter@gmail.com>
2024-02-08 09:05:53 +0100

    Update the latest courses on the homepage

--- apps/course/src/app/features/home/homepage/latest-courses/homepage-latest-courses.component.ts
+++ apps/course/src/app/features/home/homepage/latest-courses/homepage-latest-courses.component.ts
@@ -4,30 +4,28 @@ import { Course } from '../../../../../../projects/types/src/lib/course.types';
 import { LoadingBarsComponent } from '../../../../shared/feedback/loading-bars/loading-bars.component';
 import { FeedbackMessageComponent } from '../../../../shared/feedback/feedback-message/feedback-message.component';
 import { RouterLink } from '@angular/router';
+import { CoursesComponent } from '../../courses/courses.component';
 
 @Component({
   selector: 'app-latest-courses',
   standalone: true,
-  imports: [LoadingBarsComponent, FeedbackMessageComponent, RouterLink],
+  imports: [LoadingBarsComponent, FeedbackMessageComponent, RouterLink, CoursesComponent],
   templateUrl: './homepage-latest-courses.component.html',
   styleUrl: './homepage-latest-courses.component.scss',
+  providers: [CoursesComponent]
 })
 export class HomepageLatestCoursesComponent {
   public courses: Course[] = [];
+  public date: any = null;
 
-  constructor(private courseService: CourseService) {
+  constructor(
+    private courseService: CourseService,
+    private coursComp: CoursesComponent
+    ) {
     this.courseService.getLatestCourses(2).subscribe((courses) => {
       this.courses = courses;
     });
-  }
-  customTimestamp(timestamp: any) {
-    let date = timestamp.toDate();
-    let mm = date.getMonth();
-    let dd = date.getDate();
-    let yyyy = date.getFullYear();
-    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dezember"]
-  
-    date =  dd + '. ' + month[mm] + ' ' + yyyy;
-    return date;
+
+    this.date = coursComp.customTimestamp;
   }
 }
