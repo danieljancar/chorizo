82cb5aa3d4136b5b4ec9d39768718da4cf4c6bc9
Daniel Jancar <danieljancar@bluewin.ch>
2024-03-04 11:00:17 +0100

    chore(adminui): init firecms

---
+++ apps/adminui/src/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDQNYrozoQ2IcY0IxTVWSqCgKh1ra4XdtE',
+  authDomain: 'chorizo-dev.firebaseapp.com',
+  projectId: 'chorizo-dev',
+  storageBucket: 'chorizo-dev.appspot.com',
+  messagingSenderId: '97016630168',
+  appId: '1:97016630168:web:c1d973e3dc5700776779ed',
+};
