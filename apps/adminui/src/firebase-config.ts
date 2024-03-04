5ad103f7c210403fa6b335a302d4d18d7e47a7c2
Daniel Jancar <112062588+danieljancar@users.noreply.github.com>
2024-03-04 11:15:14 +0100

    Delete apps/adminui/src/firebase-config.ts

--- apps/adminui/src/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDQNYrozoQ2IcY0IxTVWSqCgKh1ra4XdtE',
-  authDomain: 'chorizo-dev.firebaseapp.com',
-  projectId: 'chorizo-dev',
-  storageBucket: 'chorizo-dev.appspot.com',
-  messagingSenderId: '97016630168',
-  appId: '1:97016630168:web:c1d973e3dc5700776779ed',
-};
