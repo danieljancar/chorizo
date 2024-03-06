918540030d33c61f4fac7bd22804d05a85c4cbde
Daniel Jancar <danieljancar@bluewin.ch>
2024-03-06 12:47:21 +0100

    fix(course): reduce y-margin on homepage tags

---
+++ apps/adminui/src/config.ts
@@ -0,0 +1,14 @@
+import { FirebaseConfig, MetaConfig } from './types/config.types.ts';
+
+export const firebaseConfig: FirebaseConfig = {
+  apiKey: 'AIzaSyDQNYrozoQ2IcY0IxTVWSqCgKh1ra4XdtE',
+  authDomain: 'chorizo-dev.firebaseapp.com',
+  projectId: 'chorizo-dev',
+  storageBucket: 'chorizo-dev.appspot.com',
+  messagingSenderId: '97016630168',
+  appId: '1:97016630168:web:c1d973e3dc5700776779ed',
+};
+
+export const metaConfig: MetaConfig = {
+  name: 'Chorizo Dev',
+};
