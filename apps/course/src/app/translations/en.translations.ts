e9275f8d70e468f55d2f7c5e6a9b8a4ecee71686
Daniel Jancar <danieljancar@bluewin.ch>
2024-06-05 11:06:54 +0200

    docs: add translation to auth service

--- apps/course/src/app/translations/en.translations.ts
+++ apps/course/src/app/translations/en.translations.ts
@@ -13,6 +13,19 @@ export const en = {
   'language-change-success': 'Language changed successfully',
   'language-change-error': 'Error changing language',
   'feedback-message.default-title': 'Oops!',
-  'feedback-message.default-message': 'Something went wrong',
+  'feedback-message.default-message': 'Something went wrong.',
   'homepage-latest-courses.error-message': 'No courses available',
+  'auth.login-success': 'Logged in successfully.',
+  'auth.register-success': 'Registered successfully.',
+  'auth.unexpected-error': 'An unexpected error occurred, please try again.',
+  'auth.email-already-in-use':
+    'This email is already in use, please try another.',
+  'auth.weak-password':
+    'The password is too weak, please try a stronger password.',
+  'auth.user-disabled':
+    'This user has been disabled. Please contact support for more information.',
+  'auth.incorrect-email-password':
+    'Incorrect email or password, please try again.',
+  'auth.too-many-requests': 'Too many requests. Please try again later.',
+  'auth.invalid-credential': 'Invalid credentials. Please try again.',
 };
